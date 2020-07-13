03cebb5f0ee0b2b544fbab8db9216e64bf80241c
jessegan <jesse13579@gmail.com>
2020-07-13 13:34:32 -0400

    Add: login and signup views and controllers

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,11 @@
+class UserController < ApplicationController
+
+    get '/users/login' do
+        erb :'users/login'
+    end
+
+    get '/users/signup' do
+        erb :'users/signup'
+    end
+
+end
