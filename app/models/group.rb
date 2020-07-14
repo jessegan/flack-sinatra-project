a5390d77366fddd5569792b27b3991f44a55b2e1
jessegan <jesse13579@gmail.com>
2020-07-14 19:37:33 -0400

    Add: make Group model Slugifiable

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ActiveRecord::Base
+    extend Slugifiable::ClassMethods
+    include Slugifiable::InstanceMethods
+
     has_many :group_users
     has_many :users, through: :group_users
 
