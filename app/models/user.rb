01f4548c3ea284d5a69811a216dd1cefe2845c32
jessegan <jesse13579@gmail.com>
2020-07-14 15:26:40 -0400

    Update: Add before_validation check on users column email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ActiveRecord::Base
     has_many :group_users
     has_many :groups, through: :group_users
 
+    before_validation :lowercase_email_before_validation
+
     validates_presence_of :email, :password, :name
     validates_uniqueness_of :email
+
+    private
+        def lowercase_email_before_validation
+            self.email.downcase!
+        end
 end
