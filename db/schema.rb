46b124ce901c98e1b8bcbeff6b16897fbba8578f
jessegan <jesse13579@gmail.com>
2020-07-14 10:52:03 -0400

    Update: add diplay_name to groups table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200713164848) do
+ActiveRecord::Schema.define(version: 20200714142225) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.string   "display_name"
+    t.boolean  "public?"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
