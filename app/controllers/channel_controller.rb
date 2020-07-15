5d44f5286ae6ea69f8e7577456ef41b9a5d1bfe4
jessegan <jesse13579@gmail.com>
2020-07-15 15:34:14 -0400

    Add: User can send messages to a channel

--- app/controllers/channel_controller.rb
+++ app/controllers/channel_controller.rb
@@ -8,4 +8,13 @@ class ChannelController < ApplicationController
         erb :'channels/view'
     end
 
+    post '/groups/:slug/c/:channel_slug' do
+        group = Group.find_by_slug(params[:slug])
+        channel = Channel.find_by_slug(params[:channel_slug])
+
+        Message.create(params[:message])
+
+        redirect to "/groups/#{group.slug}/c/#{channel.slug}"
+    end
+
 end
