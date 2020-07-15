ec635ea17f4faa41818c8dcacf5c23e9c0b6e87a
jessegan <jesse13579@gmail.com>
2020-07-15 16:41:15 -0400

    Add: User can edit channel name

--- app/controllers/channel_controller.rb
+++ app/controllers/channel_controller.rb
@@ -14,6 +14,14 @@ class ChannelController < ApplicationController
         erb :'channels/view'
     end
 
+    get '/groups/:slug/c/:channel_slug/edit' do
+        @group = Group.find_by_slug(params[:slug])
+        @channel = Channel.find_by_slug(params[:channel_slug])
+        @user = current_user
+
+        erb :'channels/edit'
+    end
+
     post '/groups/:slug/c/:channel_slug' do
         group = Group.find_by_slug(params[:slug])
         channel = Channel.find_by_slug(params[:channel_slug])
