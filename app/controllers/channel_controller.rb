42e1363372c5c5fa9247b55bf61ace017eccb1e3
jessegan <jesse13579@gmail.com>
2020-07-15 16:30:03 -0400

    Add: User can create new channels

--- app/controllers/channel_controller.rb
+++ app/controllers/channel_controller.rb
@@ -1,5 +1,11 @@
 class ChannelController < ApplicationController
 
+    get '/groups/:slug/c/new' do
+        @group = Group.find_by_slug(params[:slug])
+
+        erb :'channels/new'
+    end
+
     get '/groups/:slug/c/:channel_slug' do
         @group = Group.find_by_slug(params[:slug])
         @channel = Channel.find_by_slug(params[:channel_slug])
@@ -15,6 +21,18 @@ class ChannelController < ApplicationController
         Message.create(params[:message])
 
         redirect to "/groups/#{group.slug}/c/#{channel.slug}"
+    end
+
+    post '/groups/:slug/c' do
+        group = Group.find_by_slug(params[:slug])
+        channel = Channel.new(params[:channel])
+
+        if channel.save
+            redirect to "/groups/#{group.slug}"
+        else
+            redirect to "/groups/#{group.slug}/c/new"
+        end
+
     end
 
 end
