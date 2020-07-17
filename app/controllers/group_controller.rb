95863c9776842186fdf8885da2bfaafb69cc421a
jessegan <jesse13579@gmail.com>
2020-07-17 12:02:48 -0400

    User can update admins in the group

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -48,7 +48,7 @@ class GroupController < ApplicationController
     get '/groups/:slug/edit' do
         @user = current_user
         @group = Group.find_by_slug(params[:slug])
-        @members = @group.group_users.reject{|m| m.user==@user}.sort_by {|m| m.user.name}
+        @members = @group.group_users.sort_by {|m| m.user.name}
 
         erb :'groups/edit'
     end
@@ -57,6 +57,11 @@ class GroupController < ApplicationController
         group = Group.find_by_slug(params[:slug])
 
         group.update(params[:group])
+
+        if params[:admins]
+            group.group_users.update_all(admin: false)
+            group.group_users.where(id:params[:admins]).update_all(admin:true)
+        end
 
         redirect to "/groups/#{group.slug}"
     end
